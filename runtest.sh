6aa5e6195bb443c6fbd9975f3d3f4d7a24415438
owenfok <71066395+owenfok@users.noreply.github.com>
2021-03-27 18:52:17 +0800

    Update runtest.sh

--- runtest.sh
+++ runtest.sh
@@ -2,3 +2,4 @@ ava tests/Bucket.ts
 mocha --exit --timeout 120000 -r ts-node/register tests/login.test.ts 
 mocha --exit --timeout 120000 -r ts-node/register tests/CloudFunction.test.ts
 mocha --exit --timeout 120000 -r ts-node/register tests/firewall.js
+mocha --exit --timeout 120000 -r ts-node/register tests/ComputeEngine.js
