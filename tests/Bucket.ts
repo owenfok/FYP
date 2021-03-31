805ebcb67c372934169a95fe505892871f04454b
owenfok <71066395+owenfok@users.noreply.github.com>
2021-03-31 16:59:04 +0800

    Update Bucket.ts

--- tests/Bucket.ts
+++ tests/Bucket.ts
@@ -1,46 +1,24 @@
 import test from 'ava';
-
 import {​​ Storage }​​ from '@google-cloud/storage'
-
 import axios from 'axios';
 
-
-
-
 const storage = new Storage();
-
-
 
 async function listBuckets() {​​
 
   try {​​
-
     const results = await storage.getBuckets();
-
-
-
     const [buckets] = results;
-
-
-
     console.log('Buckets:');
 
     buckets.forEach((bucket: {​​ name: any; }​​) => {​​
-
       console.log(bucket.name);
-
     }​​);
-
     return true;
-
   }​​ catch (err) {​​
-
     console.error('ERROR:', err);
-
     return false
-
   }​​
-
 }​​
 
 
@@ -50,53 +28,24 @@ export async function saveToGCP(path: string, filename: string) {​​
   const bucketName = "testadianbucket"
 
   let response = await storage.bucket(bucketName).upload(path);
-
-  
-
   console.log(`${​​path}​​ uploaded to ${​​bucketName}​​.`);
-
   return "gs://testadianbucket/" + filename;
-
-  
-
-
-
 }​​
-
-
 
 export async function runModel(path: string) {​​
 
   let response = await axios.post("https://us-central1-testinglearningproject.cloudfunctions.net/app/get-poses", {​​ image: path }​​);
-
   return response.data;
-
-
-
 }​​
-
-
 
 test('get gcp to save file', async (t) => {​​
 
-
-
   if (await listBuckets()) {​​
-
     await saveToGCP("tests/CYRUSWONG.png","CYRUSWONG.png");
-
     t.assert("CYRUSWONG");
-
   }​​ else {​​
-
     t.log("authentication failed")
-
     console.log("authentication failed")
-
     t.assert("CYRUSWONG");
-
   }​​
-
-
-
 }​​)
