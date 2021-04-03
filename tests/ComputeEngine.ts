92b31774419f0dc39f21a336cdee0056fcdb5f79
owenfok <71066395+owenfok@users.noreply.github.com>
2021-04-04 06:11:53 +0800

    Update ComputeEngine.ts

--- tests/ComputeEngine.ts
+++ tests/ComputeEngine.ts
@@ -3,34 +3,20 @@ import * as gcp from "@pulumi/gcp";
 import "mocha";
 import { expect } from "chai";
 
-async function listInstance() {​​
 
-  try {​​
-    const results = await gcp.compute.getInstance();
-    const gcn = results;
 
-    console.log('Instance:');
+describe("Comput Engine", () => {
 
-    ((gcn: {​​ name: any; }​​) => {​​
-      console.log(gcn.name);
-    }​​);
-    return true;
-  }​​ catch (err) {​​
-    console.error('ERROR:', err);
-    return false
-  }​​
-}
+    it("One Comput Engine instance ", async () => {
 
-describe("", () => {
-   
-    it("Create instance", async () => {
-        const gcn = gcp.compute.getInstance({            
-            name:"vm",
-            zone:"asia-east2"
-        })
-        await 
-        expect("Shoule have instance " )
-    });
-    
-     
+        const WebServer = gcp.compute.getInstance({
+            name: "vm",
+            zone: "asia-east2",
+        });
+
+        expect(1,"should have instance").to.equal(
+            WebServer.catch.name![0]
+        );
+    })
 })
+
