fc3ae2f35fc8dadff2b6bef39fe2debd2ebf6fa0
william <princewilliam1998@gmail.com>
2021-04-06 22:51:45 +0800

    Create sql.tf

---
+++ tests/Tenplate/sql.tf
@@ -0,0 +1,14 @@
+resource "google_sql_database" "database" {
+  name     = "my-database"
+  instance = google_sql_database_instance.instance.name
+}
+
+resource "google_sql_database_instance" "instance" {
+  name   = "my-database-instance"
+  region = "us-central1"
+  settings {
+    tier = "db-f1-micro"
+  }
+
+  deletion_protection  = "true"
+}
