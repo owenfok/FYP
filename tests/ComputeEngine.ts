e7f0855fad3cd2f6929f0550944b4c5c5e006ad3
owenfok <71066395+owenfok@users.noreply.github.com>
2021-04-14 08:18:14 +0800

    Update ComputeEngine.ts

--- tests/ComputeEngine.ts
+++ tests/ComputeEngine.ts
@@ -1,44 +1,52 @@
-import * as pulumi from "@pulumi/pulumi";
-import * as gcp from "@pulumi/gcp";
-import "mocha";
-import { expect } from "chai";
+import * as pulumi from "@pulumi/pulumi"
+import * as gcp from "@pulumi/gcp"
+import "mocha"
+import { expect } from "chai"
 
 
-describe("Comput Engine", async () => {
-    const computeInstance = pulumi.output(gcp.compute.getInstance({
-        name: "vm",
-        zone: "asia-east2-a"
-        
-    },{async:true}));
+describe("Comput Engine", () => {
+    ////////////////////////////////////////////////////////////////////////////   
+    //   const gcn = gcp.compute.getInstance({
+    // name: "vm"
+    //});
+    //const instanceZone = gcn.then(gcn => gcn.zone)
+    //const instanceBootDisk =  gcn.then(gcn => gcn.bootDisks)
+    //const instanceDeletionProtestion =  gcn.then(gcn => gcn.deletionProtection)
+    /////////////////////////////////////////////////////////////////////////////
 
-    const machineName = computeInstance.machineType
-    const instanceZone = computeInstance.zone
-    const instanceBootDisk = computeInstance.bootDisks
-    const instanceDeletionProtestion = computeInstance.deletionProtection
+    const gcn = pulumi.output(gcp.compute.getInstance({
+        name: "vm"
+    }, { async: true }))
 
-
-    it("Machine type is f1 mirco ", async () => {
-        expect(machineName, "should have instance").to.equal("f1-mirco");
+    it("Machine type using f1 micro ", () => {
+        const machineName = gcn.machineType
+        expect(machineName, "Machine type should is f1 mirco").to.equal("f1-micro");
+        console.log(machineName)
     })
 
-    it("Zone in asia-east2-a ", async () => {
-        expect(instanceZone, "should have instance").to.equal("asia-east2-a");
+    it("Zone in asia-east2-a ", () => {
+        const instanceZone = gcn.zone
+        expect(instanceZone, "should in asia-east2-a").to.equal("asia-east2-a");
+        console.log(instanceZone)
     })
 
-    it("Instance BootDisk is ubuntu", async () => {
-        expect(instanceBootDisk, "should have instance").to.equal("ubuntu-1804-bionic-v20210412");
+    it("Instance BootDisk is ubuntu", () => {
+        const instanceBootDisk = gcn.bootDisks
+        expect(instanceBootDisk, "BootDisk should is ubuntu").to.equal("ubuntu-1804-bionic-v20210412");
+        console.log(instanceBootDisk)
     })
-        
-    it("Disable instance delete protection ", async () => {
-        expect(instanceDeletionProtestion, "should have instance").to.equal(false);
+
+    it("Disable instance delete protection ", () => {
+        const instanceDeletionProtestion = gcn.deletionProtection
+        expect(instanceDeletionProtestion, "should disable instance delete protection").to.equal(false);
+        console.log(instanceDeletionProtestion)
     })
 
 
-    //console.log(computeInstance)
+    console.log(gcn)
     //console.log(machineName)
     //console.log(instanceZone)
-    //console.log(instanceBootDisk)
-
-
+    // console.log(instanceBootDisk)
+    // console.log(instanceDeletionProtestion)
 
 })
