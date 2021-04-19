15dedd0cd22694ad77b922be757d416627264204
owenfok <71066395+owenfok@users.noreply.github.com>
2021-04-20 07:46:39 +0800

    Update ComputeEngine.ts

--- tests/ComputeEngine.ts
+++ tests/ComputeEngine.ts
@@ -1,9 +1,14 @@
+import * as pulumi from "@pulumi/pulumi";
 import { expect } from "chai"
 import "mocha"
-import * as stack from "./stack.json"
+import * as stack from "../stack.json"
 
 const a = stack.deployment.resources
+
 const b = a[0].outputs
+
+
+
 //console.log(b)
 
 describe("Comput Engine", () => {
@@ -18,6 +23,16 @@ describe("Comput Engine", () => {
 
     it("Disable instance delete protection ", () => {
         expect(b.instanceDeletionProtestion, "should disable instance delete protection").to.equal(false);
+    })
+
+    it("The OS is debian 10 ", () => {
+
+        expect(b.instanceImage, "should is debian 10").to.equal("https://www.googleapis.com/compute/beta/projects/debian-cloud/global/images/debian-10-buster-v20210316");
+    })
+
+    it("The drive size is 10 GB  ", () => {
+
+        expect(b.instanceSize, "should is 10 GB").to.equal(10);
     })
 
 })
