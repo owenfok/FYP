82dc7d3d270cebb91b28abe0a023fb7ecfaba875
owenfok <71066395+owenfok@users.noreply.github.com>
2021-04-20 09:24:16 +0800

    Update index.ts

--- index.ts
+++ index.ts
@@ -2,25 +2,18 @@ import * as pulumi from "@pulumi/pulumi"
 import * as gcp from "@pulumi/gcp"
 import "mocha"
 
-
 // Instance 
 const gcn = pulumi.output(gcp.compute.getInstance({
     name: "vm",
     zone: "asia-east2-a"
 }, { async: true }))
 
-
 export const instanceZone = gcn.zone
-//console.log(getZone)
 export const machineName = gcn.machineType
-//console.log(machineName)
 export const instanceDeletionProtestion = gcn.deletionProtection
-//console.log(instanceDeletionProtestion)
-export const instanceBootDisk = gcn.bootDisks
-//console.log(instanceBootDisk)
-
+export const instanceImage = gcn.bootDisks[0].initializeParams[0].image
+export const instanceSize = gcn.bootDisks[0].initializeParams[0].size
 //
-
 
 // InstanceGroup
 const InstanceGroup = pulumi.output(gcp.compute.getInstanceGroup({
@@ -30,42 +23,36 @@ const InstanceGroup = pulumi.output(gcp.compute.getInstanceGroup({
 
 export const InstanceGroupName = InstanceGroup.name
 export const InstanceGroupSize = InstanceGroup.size
-//console.log(InstanceGroupSize)
-
 //
 
-
 // Instance Template
-
 const InstanceTemplate = pulumi.output(gcp.compute.getInstanceTemplate({
     name: "instance-template-1",
     project: ""
 }, { async: true }));
 
 export const InstanceTemplateName = InstanceTemplate.name
-//console.log(InstanceTemplateName)
 export const InstanceTemplateMetadata = InstanceTemplate.metadata
-//console.log(InstanceTemplateMetadata)
 export const InstanceTemplateMachineType = InstanceTemplate.machineType
-//console.log(InstanceTemplateMachineType)
-
-
+//
 
 //  Identity and Access Management
-
-
-const roleinfo = gcp.iam.getRule({
+const roleinfo = pulumi.output(gcp.iam.getRule({
     name: "roles/compute.viewer",
-});
-export const theRoleId = roleinfo.then(roleinfo => console.log(roleinfo.id))
-export const theRoleName = roleinfo.then(roleinfo => console.log(roleinfo.name))
-export const theRoleStage = roleinfo.then(roleinfo => console.log(roleinfo.stage))
-export const theRolePermissions = roleinfo.then(roleinfo => console.log(roleinfo.includedPermissions))
+}, { async: true }));
 
+export const theRoleId = roleinfo.id
+export const theRoleName = roleinfo.name
+export const theRoleStage = roleinfo.stage
+export const theRolePermissions = roleinfo.includedPermissions
+//
 
+// pubsub
+const pubsubTopic = pulumi.output(gcp.pubsub.getTopic({
+    name: "abc",
+}, { async: true }));
 
-const roleinfos = pulumi.output(gcp.iam.getRule({
-    name: "roles/compute.viewer",
-}));
-
-export const a = roleinfos.id
+export const pubsubTopicName = pubsubTopic.name
+export const pubsubTopicLabels = pubsubTopic.labels
+export const pubsubTopicMessageStoragePolicies = pubsubTopic.messageStoragePolicies
+//
