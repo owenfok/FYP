b7460efcbd3ef8240f5cdcf353716937d4d16a9a
owenfok <71066395+owenfok@users.noreply.github.com>
2021-04-20 21:00:50 +0800

    Update InstanceTemplate.ts

--- tests/InstanceTemplate.ts
+++ tests/InstanceTemplate.ts
@@ -1,23 +1,21 @@
-import * as pulumi from "@pulumi/pulumi";
+import  { datas} from "../ComputeEngine"
 import { expect } from "chai"
 import "mocha"
-import * as stack from "../stack.json"
 
-const data = stack.deployment.resources[0].outputs
 
 describe("Instance template", () => {
     it("Machine type using f1 micro ", () => {
-        expect(data.InstanceTemplateMachineType, "Machine type should is f1 mirco").to.equal('f1-micro');
+        expect(datas["InstanceTemplateMachineType"], "Machine type should is f1 mirco").to.equal('f1-micro');
     })
 
 
     it("Instance Template Metadata ", () => {
-        expect(data.InstanceTemplateMetadata, "should have metadata").to.equal("sudo vi test.txt");
+        expect(datas["InstanceTemplateMetadata"], "should have metadata").to.equal("sudo touch test.txt");
     })
 
     it("Instance template name ", () => {
 
-        expect(data.InstanceTemplateName, " Name should is instance-template-1 ").to.equal("instance-template-1");
+        expect(datas["InstanceTemplateName"], " Name should is instance-template-1 ").to.equal("instance-template-1");
     })
 
 })
