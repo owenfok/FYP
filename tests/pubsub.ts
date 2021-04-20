7abfc85f77e6d712ab073f5deecc29630ebaa566
owenfok <71066395+owenfok@users.noreply.github.com>
2021-04-20 10:19:21 +0800

    Update pubsub.ts

--- tests/pubsub.ts
+++ tests/pubsub.ts
@@ -8,14 +8,6 @@ const data = stack.deployment.resources[0].outputs
 
 describe("PubSub", () => {
     
-    it("Machine type using f1 micro ", () => {
-        expect(data.pubsubTopicLabels, "Machine type should is f1 mirco").to.equal("","");
-    })
-    
-    it("Topic Message Storage Policies", () => {
-        expect(data.pubsubTopicMessageStoragePolicies, "should have Topic Message Storage Policies").to.equal("asia-east2-a");
-    })
-    
     it("Topic name is abc", () => {
         expect(data.pubsubTopicName, "Name should is abc").to.equal("abc");
     })
