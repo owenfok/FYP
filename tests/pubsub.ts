0e4cd164ade6eeab2c1d42a995d41fbc1c29cdbe
owenfok <71066395+owenfok@users.noreply.github.com>
2021-04-20 09:31:16 +0800

    Update pubsub.ts

--- tests/pubsub.ts
+++ tests/pubsub.ts
@@ -5,28 +5,19 @@ import * as stack from "./stack.json"
 
 const data = stack.deployment.resources[0].outputs
 
-describe("Comput Engine", () => {
+
+describe("PubSub", () => {
+    
     it("Machine type using f1 micro ", () => {
-        expect(data.machineName, "Machine type should is f1 mirco").to.equal('f1-micro');
+        expect(data.pubsubTopicLabels, "Machine type should is f1 mirco").to.equal("","");
     })
-
-
-    it("Zone in asia-east2-a ", () => {
-        expect(data.instanceZone, "should in asia-east2-a").to.equal("asia-east2-a");
+    
+    it("Topic Message Storage Policies", () => {
+        expect(data.pubsubTopicMessageStoragePolicies, "should have Topic Message Storage Policies").to.equal("asia-east2-a");
     })
-
-    it("Disable instance delete protection ", () => {
-        expect(data.instanceDeletionProtestion, "should disable instance delete protection").to.equal(false);
-    })
-
-    it("The OS is debian 10 ", () => {
-
-        expect(data.instanceImage, "should is debian 10").to.equal("https://www.googleapis.com/compute/beta/projects/debian-cloud/global/images/debian-10-buster-v20210316");
-    })
-
-    it("The drive size is 10 GB  ", () => {
-
-        expect(data.instanceSize, "should is 10 GB").to.equal(10);
+    
+    it("Topic name is abc", () => {
+        expect(data.pubsubTopicName, "Name should is abc").to.equal("abc");
     })
 
 })
