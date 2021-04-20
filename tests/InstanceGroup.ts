2d400a3c71b8a909c1400aa3b397c255910d2bcf
owenfok <71066395+owenfok@users.noreply.github.com>
2021-04-20 20:58:44 +0800

    Update InstanceGroup.ts

--- tests/InstanceGroup.ts
+++ tests/InstanceGroup.ts
@@ -1,17 +1,16 @@
+import  { datas} from "../ComputeEngine"
 import { expect } from "chai"
 import "mocha"
-import * as stack from "../stack.json"
 
-const data = stack.deployment.resources[0].outputs
 
 describe("Instance group", () => {
     it("'instance group name is instance-group-1", () => {
-        expect(data.InstanceGroupName, "Name should is instance-group-1 ").to.equal('instance-group-1');
+        expect(datas["InstanceGroupName"], "Name should is instance-group-1 ").to.equal('instance-group-1');
     })
 
 
     it("instance group size min 1 and max 1", () => {
-        expect(data.InstanceGroupSize, "should min 1 and max 1 ").to.equal(1);
+        expect(datas["InstanceGroupSize"], "should min 1 and max 1 ").to.equal(1);
     })
 
 })
