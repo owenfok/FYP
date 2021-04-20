c09f48077379887da9e28566e28aa3a6e254484d
owenfok <71066395+owenfok@users.noreply.github.com>
2021-04-20 09:16:19 +0800

    Update ComputeEngine.ts

--- tests/ComputeEngine.ts
+++ tests/ComputeEngine.ts
@@ -1,19 +1,13 @@
 import * as pulumi from "@pulumi/pulumi";
 import { expect } from "chai"
 import "mocha"
-import * as stack from "../stack.json"
+import * as stack from "./stack.json"
 
-const a = stack.deployment.resources
-
-const b = a[0].outputs
-
-
-
-//console.log(b)
+const data = stack.deployment.resources[0].outputs
 
 describe("Comput Engine", () => {
     it("Machine type using f1 micro ", () => {
-        expect(b.machineName, "Machine type should is f1 mirco").to.equal('f1-micro');
+        expect(data.machineName, "Machine type should is f1 mirco").to.equal('f1-micro');
     })
 
 
@@ -22,17 +16,17 @@ describe("Comput Engine", () => {
     })
 
     it("Disable instance delete protection ", () => {
-        expect(b.instanceDeletionProtestion, "should disable instance delete protection").to.equal(false);
+        expect(data.instanceDeletionProtestion, "should disable instance delete protection").to.equal(false);
     })
 
     it("The OS is debian 10 ", () => {
 
-        expect(b.instanceImage, "should is debian 10").to.equal("https://www.googleapis.com/compute/beta/projects/debian-cloud/global/images/debian-10-buster-v20210316");
+        expect(data.instanceImage, "should is debian 10").to.equal("https://www.googleapis.com/compute/beta/projects/debian-cloud/global/images/debian-10-buster-v20210316");
     })
 
     it("The drive size is 10 GB  ", () => {
 
-        expect(b.instanceSize, "should is 10 GB").to.equal(10);
+        expect(data.instanceSize, "should is 10 GB").to.equal(10);
     })
 
 })
