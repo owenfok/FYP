7bbda52f7decfb85b076458a9f2e192371a6ff6c
owenfok <71066395+owenfok@users.noreply.github.com>
2021-04-22 14:09:52 +0800

    Delete runtest.sh

--- runtest.sh
+++
@@ -1,5 +0,0 @@
-ava tests/Bucket.ts 
-mocha --exit --timeout 120000 -r ts-node/register tests/login.test.ts 
-mocha --exit --timeout 120000 -r ts-node/register tests/CloudFunction.test.ts
-mocha --exit --timeout 120000 -r ts-node/register tests/firewall.js
-mocha --exit --timeout 120000 -r ts-node/register tests/ComputeEngine.js
