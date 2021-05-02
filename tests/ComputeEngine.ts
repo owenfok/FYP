e9ff84969f877f90467e9af49d109f4a7d8fdb85
owenfok <71066395+owenfok@users.noreply.github.com>
2021-05-03 00:12:49 +0800

    Update ComputeEngine.ts

--- tests/ComputeEngine.ts
+++ tests/ComputeEngine.ts
@@ -3,7 +3,7 @@ import { expect } from "chai"
 import "mocha"
 
 
-describe("Comput Engine", () => {
+describe("Compute Engine", () => {
     it("Machine type using f1 micro ", () => {
         expect(datas["machineName"], "Machine type should is f1 mirco").to.equal('f1-micro');
     })
