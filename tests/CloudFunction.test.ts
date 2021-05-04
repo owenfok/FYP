d991e51a4318ba66538fd1b523d332152247446c
AdianWong <61007771+AdianWong@users.noreply.github.com>
2021-05-04 12:33:02 +0800

    Cloud Function new version

--- tests/CloudFunction.test.ts
+++ tests/CloudFunction.test.ts
@@ -1,31 +1,70 @@
-import * as Supertest from 'supertest'
-import { deepStrictEqual } from 'assert'
-import * as dotenv from 'dotenv'
+import { datas } from "../stack"
+import { expect } from "chai"
+import "mocha";
+import * as chai from "chai";
+import chaiString from "chai-string";
+chai.use(chaiString);
 
-dotenv.config()
 
-const supertest = Supertest(process.env.GCP_SERVERLESS_URL)
+describe("CloudFunction Name", () => {
+    it("Check CloudFunction Name 'gcplab' .", async () => {
+        expect(datas["GetCloudFunction"]).to.equal("gcplab");
+    });
+});
 
-it('CloudFunction json entry point', async () => {
-    await supertest
-        .get('/helloWorldHTTP')
-        .expect(200)
-        .expect('Content-Type', 'application/json; charset=utf-8')
-        .expect(response => {
-            deepStrictEqual(
-                response.text,
-                JSON.stringify({ result: 'Hello World!' })
-            )
-        })
-})
 
-it('should send echo back json', async () => {
-    await supertest
-        .post('/helloWorldHTTP')
-        .send({ echo: 'test1' })
-        .expect(200)
-        .expect('Content-Type', 'application/json; charset=utf-8')
-        .expect(response => {
-            deepStrictEqual(response.text, JSON.stringify({ echo: 'test1' }))
-        })
-})
+describe("CloudFunction Region", () => {
+    it("Check CloudFunction Region 'us-east1' .", async () => {
+        expect(datas["GetCloudFunctionRegion"]).to.equal("us-east1");
+    });
+});
+
+
+describe("CloudFunction Entry Point set 'hello_pubsub' ", () => {
+    it("Check CloudFunction Entry Point 'hello_pubsub' .", async () => {
+        expect(datas["GetCloudFunctionEntryPt"]).to.equal("hello_pubsub");
+    });
+});
+
+
+describe("Use pubsub to trigger", () => {
+    it("Set CloudFunction trigger publish .", async () => {
+        expect(datas["GetCloudFunctionTriggers"]).to.equal("google.pubsub.topic.publish");
+    });
+
+    it("Set CloudFunction failure Policies 'false' .", async () => {
+        expect(datas["GetCloudFunctionPolicies"]).to.equal(false);
+    });
+
+    it("Set PubSub In 'gcr' .", async () => {
+        expect(datas["GetCloudFunctionResource"]).to.endsWith("topics/gcr");
+    });
+});
+
+
+
+describe("CloudFunction runtime python3.9 ", () => {
+    it("Check CloudFunction runtime python3.9 .", async () => {
+        expect(datas["GetCloudFunctionRuntime"]).to.equal("python39");
+    });
+});
+
+
+describe("CloudFunction set timeout 60 seconds.  ", () => {
+    it("CloudFunction set timeout 60 seconds .", async () => {
+        expect(datas["GetCloudFunctionTimeout"]).to.equal(60);
+    });
+});
+
+
+describe("CloudFunction set VpcConnector, 'gcplab'  ", () => {
+    it("CloudFunction set VpcConnector, 'gcplab' .", async () => {
+        expect(datas["GetCloudFunctionVpcConnector"]).to.equal("gcplab");
+    });
+});
+
+describe("CloudFunction set VpcConnector Egress Private Ranges Only  ", () => {
+    it("CloudFunction set VpcConnector Egress PRIVATE RANGES ONLY .", async () => {
+        expect(datas["GetCloudFunctionVPCsetting"]).to.equal("PRIVATE_RANGES_ONLY");
+    });
+});
