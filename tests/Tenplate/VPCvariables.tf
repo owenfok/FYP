24f359d1b6e9c07ed2a6134b71311f9ae0cb05cb
AdianWong <61007771+AdianWong@users.noreply.github.com>
2021-05-06 22:02:37 +0800

    Add files via upload

---
+++ tests/Tenplate/VPCvariables.tf
@@ -0,0 +1,23 @@
+variable "name_prefix" {
+    type = string
+}
+
+variable "project" {
+    type = string
+}
+
+variable "region" {
+    type = string
+}
+
+variable "cidr_block" {
+    type = string
+}
+
+variable "cidr_subnetwork_width_delta" {
+    type = number
+}
+
+variable "cidr_subnetwork_spacing" {
+    type = number
+}
