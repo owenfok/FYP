90ca8f8bee8125c88d164d85aa8778acdb58b156
owenfok <71066395+owenfok@users.noreply.github.com>
2021-05-06 17:38:00 +0800

    Update monitoring.ts

--- tests/monitoring.ts
+++ tests/monitoring.ts
@@ -3,16 +3,16 @@ import { expect } from "chai"
 import "mocha"
 
 describe("Try to set alert", () => {
-    it("should have email", async () => {
+    it("should have email",  () => {
         expect(datas["notiType"], "Should have email").to.equal("Email");
     })
-    it("Noticification channel should have name 'gcp noti'", async () => {
+    it("Noticification channel should have name 'gcp noti'",  () => {
         expect(datas["notiDisplayName"], "Display name should be 'gcp noti'").to.equal("gcp noti");
     })
-    it("Noticification channel should be enabled", async () => {
+    it("Noticification channel should be enabled",  () => {
         expect(datas["notiStatus"], "Status should be enabled").to.equal(true);
     })
-    it("At least 1 contact should be set", async () => {
+    it("At least 1 contact should be set",  () => {
         expect(datas["notiLabels"], "At least 1 contact should be set").to.equal(true);
     })
 })
